3edfbe4afaed457a4e37bfd6a3e9db585f404163
KimHyunJun <kdhc7777@naver.com>
2026-09-10 12:16:16 +0900

    chore(project): initialize Spring Boot project

---
+++ src/test/java/com/hyunjun/backend/BackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunjun.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
